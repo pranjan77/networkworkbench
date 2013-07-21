6d419412968884d4412b2981429fec71d452f655
Priya Ranjan <pranjan77@gmail.com>
2013-07-21 20:21:04 +0000

    Basic UI all done

--- app.js
+++ app.js
@@ -11,18 +11,134 @@ var express = require('express')
 
 var app = express();
 
+// Upload related stuff
+
+var fs = require('fs'),
+    mkdirp = require('mkdirp');
+
+// Directories for uploaded files
+var publicFolder = '/public/';
+var uploadFolder = 'uploads';
+if (!fs.existsSync(__dirname + publicFolder + uploadFolder )) mkdirp.sync(__dirname + publicFolder + uploadFolder);
+
+var tmpUpload = '/public/uploads/tmp/';
+if (!fs.existsSync(__dirname + tmpUpload)) mkdirp.sync(__dirname + tmpUpload);
+
+var uploadDirBase = '/public/uploads/actual/';
+if (!fs.existsSync(__dirname + uploadDirBase)) mkdirp.sync(__dirname + uploadDirBase);
+
 // all environments
 app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(express.session({key: 'express.sid'}));
+app.use(express.bodyParser({
+	uploadDir: __dirname + tmpUpload,
+	keepExtensions: true
+}));
+app.use(express.methodOverride());
+app.use(function(req, res, next){
+  res.locals.session = req.session;
+  next();
+});
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+uploadHandler = function( req, res, next ) {
+  var fileObj = req.files.file;
+  var tmpPath = fileObj.path;
+  var targetPath = __dirname + uploadDirBase + req.sessionID;
+
+  if (!fs.existsSync(targetPath)) {
+    mkdirp.sync(targetPath);
+  }
+  var targetFile = __dirname + uploadDirBase + req.sessionID + '/' + fileObj.name;
+  var attributeFile = __dirname + uploadDirBase + req.sessionID + '/' + fileObj.name + '.attrib';
+  var DBPATH = '/' + uploadFolder + '/actual/' + req.sessionID + '/' + fileObj.name;
+  var DBNAME = fileObj.name;
+  res.locals.DBPATH = DBPATH;
+  res.locals.DBNAME = DBNAME;
+  res.locals.ATTRIB = attributeFile;
+  res.locals.TARGET = targetFile;
+
+  fs.rename(tmpPath, targetFile, function(err) {
+    if (err) next(err);
+    fs.unlink(tmpPath, function() {
+      if (err) throw err;
+        next();
+    });
+  });
+};
+
+auth = function(req, res, next) {
+
+	if (req.session.authenticated == null ||  
+			req.session.authenticated == undefined ||  
+			req.session.authenticated == false ) { 
+
+		req.session.authenticated = true;
+		var now = new Date();
+
+		var experiment = {
+			sessionId : req.sessionID,
+			userName : '',
+			userGlobusToken: '',
+			startTime : now,
+			step1 : {
+				email : '',
+				expTable: {
+					fileName: '',
+					shockId: ''
+				},
+				sampleId: {
+					fileName: '',
+					shockId: ''
+				},
+				geneAnno: {
+					fileName: '',
+					shockId: ''
+				}
+			},
+			step2 : {
+				execurationStarted: false,
+				startTime : null,
+				notificationSent : false,
+				AWEInput: {
+					fileName: '',
+					shockId: ''
+				},
+				visited: false
+			},
+			step3 : {
+				status: 'not started',
+				visited: false,
+				refreshCount: 0,
+				sessionId: req.sessionID
+			},
+			step4 : {
+				execurationResult: false,
+				notificationSent : false,
+				endTime : null,
+				visited: false
+			},
+			step5 : {
+				downloadFile: '',
+				downloadFileURL: '',
+				visited: false
+			}
+		};
+		req.session.data = experiment;
+		res.locals.session = req.session;
+		next();
+	} else {
+		res.locals.session = req.session;
+		next();
+	}
+
+};
 
 // development only
 if ('development' == app.get('env')) {
@@ -30,10 +146,12 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/nw/step1',  routes.nwStep1);
+app.get('/nw/step1', auth,  routes.nwStep1);
 app.get('/nw/step2',  routes.nwStep2);
 app.get('/nw/step3',  routes.nwStep3);
 app.get('/nw/step4',  routes.nwStep4);
+
+app.post('/uploadFileAction', auth, uploadHandler, routes.uploadFileAction);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,60 @@
 
-/*
- * GET home page.
- */
+var fs = require('fs');
+var rest = require('restler');
+var dateFormat = require('dateformat');
 
 exports.index = function(req, res){
-    res.render("index", { title: "Networks Workbench", js: "network/main" });
+	res.render("index", { title: "Networks Workbench", js: "network/main" });
+};
+
+exports.uploadFileAction = function(req, res){
+
+	var url = 'http://140.221.84.236:8000/node';
+	var un = 'kbasetest';
+	var ps = '@Suite525';
+
+	var session = res.locals.session;
+	var experiment = session.data;
+	var sessionId = experiment.sessionId;
+
+	var attributes = {};
+	attributes.experiment_type = 'coexpression';
+	attributes.experiment_id = sessionId;
+	attributes.filetype = 'expressionTable';
+
+	var fd = fs.openSync(res.locals.ATTRIB, 'a+', 0666);
+	fs.writeSync(fd, JSON.stringify(attributes));
+	fs.closeSync(fd);
+
+	rest.post(url, {
+		multipart: true,
+		username: un,
+		password: ps,
+		data: {
+			'attributes': rest.file(res.locals.ATTRIB),
+		'upload': rest.file(res.locals.TARGET)
+		}
+	}).on('complete', function(data) {
+		console.dir(data);
+		res.send(200, req.files);
+	});
+
 };
 
 exports.nwStep1 = function (req, res) {
-    res.render("networkStep", { step: 1 });
+	res.render("networkStep", { step: 1 });
 };
 
 exports.nwStep2 = function (req, res) {
-    res.render("networkStep", { step: 2 });
+	res.render("networkStep", { step: 2 });
 };
 
 exports.nwStep3 = function (req, res) {
-    res.render("networkStep", { step: 3 });
+	res.render("networkStep", { step: 3 });
 };
 
 exports.nwStep4 = function (req, res) {
-    res.render("networkStep", { step: 4 });
+	res.render("networkStep", { step: 4 });
 };
 
 
